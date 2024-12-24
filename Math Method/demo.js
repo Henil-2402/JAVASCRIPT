3692a91e47ed842e8d82d869b5555ec5d73b42d6
Henil-2402 <henilkayastha2002@gmail.com>
2024-12-24 13:31:28 +0530

    add

---
+++ Math Method/demo.js
@@ -0,0 +1,8 @@
+console.log(Math.abs(-9)) //-v to +v number
+console.log(Math.round(3.6)) //
+console.log(Math.round(3.2))
+console.log(Math.ceil(3.2))
+console.log(Math.floor(3.2))
+console.log(Math.sqrt(9))
+console.log(Math.random())
+console.log(Math.ceil(Math.random()*9999+10000))
