dbf0138f29fcb5f94e55e5a20b5eaffbedfbb6b5
Henil-2402 <henilkayastha2002@gmail.com>
2024-12-24 13:37:50 +0530

    completed

--- Math Method/demo.js
+++ Math Method/demo.js
@@ -1,8 +1,8 @@
 console.log(Math.abs(-9)) //-v to +v number
-console.log(Math.round(3.6)) //
-console.log(Math.round(3.2))
-console.log(Math.ceil(3.2))
-console.log(Math.floor(3.2))
-console.log(Math.sqrt(9))
-console.log(Math.random())
-console.log(Math.ceil(Math.random()*9999+10000))
+console.log(Math.round(3.6)) // round of to 4
+console.log(Math.round(3.2)) //round of to 3
+console.log(Math.ceil(3.2)) // 1 more
+console.log(Math.floor(3.2)) // 1 less 
+console.log(Math.sqrt(9))   //square ro0t
+console.log(Math.random()) //generate random number
+console.log(Math.ceil(Math.random()*9999+10000)) // remove decimal and generate exact 5 digit random number
